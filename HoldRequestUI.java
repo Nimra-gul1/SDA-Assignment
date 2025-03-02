e870262f06a8d672753838c00981d508e0285e37
Nimra-gul1 <nimraofficial121@gmail.com>
2025-03-02 14:32:07 +0500

    it doesnt violate any principle and follows SRP

---
+++ HoldRequestUI.java
@@ -0,0 +1,25 @@
+package LMS;
+
+public class HoldRequestUI {
+    /*
+       SRP FIX: This class handles ui-related logic.
+       Moved `printHoldRequests` from `HoldRequestOperations`.
+       Now the operations class is more cleaner and more reusable.
+    */
+
+    public static void printHoldRequests(HoldRequestOperations holdRequestOperations) {
+        if (!holdRequestOperations.getHoldRequests().isEmpty()) {
+            System.out.println("\nHold Requests:");
+            System.out.println("------------------------------------------------------------");
+            System.out.println("No.\t\tBook Title\t\tBorrower's Name\t\tRequest Date");
+            System.out.println("------------------------------------------------------------");
+
+            for (int i = 0; i < holdRequestOperations.getHoldRequests().size(); i++) {
+                System.out.print(i + "-" + "\t\t");
+                holdRequestOperations.getHoldRequests().get(i).print();
+            }
+        } else {
+            System.out.println("\nNo Hold Requests.");
+        }
+    }
+}
